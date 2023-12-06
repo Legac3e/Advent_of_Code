ade039a9d9856cdade51bc70edc9e2dfee90ee03
Jake Duckett <legac3e@gmail.com>
2023-12-06 16:30:36 -0700

    Finished day 01 part 1 and 2.

---
+++ 2023/Day_01/main.cpp
@@ -0,0 +1,166 @@
+// https://adventofcode.com/2023/day/1
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cctype>
+
+const char* FILE_PART1 = "input.txt";
+
+struct calibrated_values {
+    char first;
+    char last;
+};
+
+void part1() {
+    std::ifstream inputFile(FILE_PART1);
+
+    if (!inputFile) {
+        std::cerr << "Failed to open the file." << std::endl;
+        return;
+    }
+
+    int sum = 0;
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        calibrated_values vals {0, 0};
+
+        for (char c : line) {
+            if (std::isdigit(c)) {
+                vals.last = c;
+                if (!vals.first) {
+                    vals.first = c;
+                }
+            }
+        }
+
+        sum += (10*(vals.first - '0')) + (vals.last - '0');
+    }
+
+    std::cout << sum << std::endl;
+}
+
+void part2() {
+    std::ifstream inputFile(FILE_PART1);
+
+    if (!inputFile) {
+        std::cerr << "Failed to open the file." << std::endl;
+        return;
+    }
+
+    int sum = 0;
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        calibrated_values vals {0, 0};
+
+        for (size_t i = 0; i < line.size(); i++) {
+            switch (std::tolower(line[i])) {
+                case 'o': { //3
+                    if ((i+2) < line.size() && line[i+1] == 'n' && line[i+2] == 'e') {
+                        vals.last = '1';
+                        if (!vals.first) {
+                            vals.first = '1';
+                        }
+                    }
+                } break;
+
+                case 't': {// 3, 5
+                    if ((i+2) < line.size() && line[i+1] == 'w' && line[i+2] == 'o') {
+                        vals.last = '2';
+                        if (!vals.first) {
+                            vals.first = '2';
+                        }
+                    }
+
+                    if ((i+4) < line.size() && line[i+1] == 'h' && line[i+2] == 'r' && line[i+3] == 'e' && line[i+4] == 'e') {
+                        vals.last = '3';
+                        if (!vals.first) {
+                            vals.first = '3';
+                        }
+                    }
+                } break;
+
+                case 'f': { //4
+                    if ((i+3) < line.size() && line[i+1] == 'o' && line[i+2] == 'u' && line[i+3] == 'r') {
+                        vals.last = '4';
+                        if (!vals.first) {
+                            vals.first = '4';
+                        }
+                    }
+
+                    if ((i+3) < line.size() && line[i+1] == 'i' && line[i+2] == 'v' && line[i+3] == 'e') {
+                        vals.last = '5';
+                        if (!vals.first) {
+                            vals.first = '5';
+                        }
+                    }
+                } break;
+
+                case 's':{ //3
+                    if ((i+2) < line.size() && line[i+1] == 'i' && line[i+2] == 'x') {
+                        vals.last = '6';
+                        if (!vals.first) {
+                            vals.first = '6';
+                        }
+                    }
+
+                    if ((i+4) < line.size() && line[i+1] == 'e' && line[i+2] == 'v' && line[i+3] == 'e' && line[i+4] == 'n') {
+                        vals.last = '7';
+                        if (!vals.first) {
+                            vals.first = '7';
+                        }
+                    }
+                } break;
+
+                case 'e': {//5
+                    if ((i+4) < line.size() && line[i+1] == 'i' && line[i+2] == 'g' && line[i+3] == 'h' && line[i+4] == 't') {
+                        vals.last = '8';
+                        if (!vals.first) {
+                            vals.first = '8';
+                        }
+                    }
+                } break;
+
+                case 'n': {//4
+                    if ((i+3) < line.size() && line[i+1] == 'i' && line[i+2] == 'n' && line[i+3] == 'e') {
+                        vals.last = '9';
+                        if (!vals.first) {
+                            vals.first = '9';
+                        }
+                    }
+                } break;
+
+                /*
+                case 'z': {//4
+                    if ((i+4) < line.size() && line[i+1] == 'e' && line[i+2] == 'r' && line[i+3] == 'o') {
+                        vals.last = '0';
+                        if (!vals.first) {
+                            vals.first = '0';
+                        }
+                    }
+                } break;
+                */
+
+                default: {
+                    if (std::isdigit(line[i])) {
+                        vals.last = line[i];
+                        if (!vals.first) {
+                            vals.first = line[i];
+                        }
+                    }
+                } break;
+            }
+        }
+
+        std::cout << vals.first << " - " << vals.last << std::endl;
+        sum += (10*(vals.first - '0')) + (vals.last - '0');
+    }
+
+    std::cout << sum << std::endl;
+}
+
+int main() {
+    // part1();
+    part2();
+}
+
