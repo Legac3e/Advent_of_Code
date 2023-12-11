b095ee9fa1ad6da3ba7b8fdaf8d3241f27f6d8aa
Jake Duckett <legac3e@gmail.com>
2023-12-10 20:42:19 -0700

    Just finished part 1 of day 6.

---
+++ 2023/Day_06/part1.cpp
@@ -0,0 +1,77 @@
+// https://adventofcode.com/2023/day/5
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+struct race_entry {
+    int time;
+    int recordDistance;
+};
+
+const char* INPUT_FILE = "input.txt";
+
+int main() {
+    std::ifstream inputfile(INPUT_FILE);
+
+    if (!inputfile.is_open()) {
+        std::cerr << "Failed to open the file:\n\t" << INPUT_FILE << std::endl;
+        exit(1);
+    }
+
+    std::vector<race_entry> races;
+
+    for (int raceNum = 0; raceNum < 2; raceNum++) {
+        int entries = 0;
+        std::string line;
+        std::getline(inputfile, line);
+
+        size_t i;
+        for (i = 0; i < line.size(); i++) {
+            if (line[i] == ':') { i += 2; break; }
+        }
+
+        for (; i < line.size(); i++) {
+            int num;
+            int len;
+
+            sscanf(&line[i], "%d%n", &num, &len);
+
+            if (raceNum == 0) {
+                races.emplace_back(race_entry{num, 0});
+            } else {
+                races[entries++].recordDistance = num;
+            }
+
+            i += len - 1;
+        }
+    }
+
+    int winProducts = 1;
+
+    for (const auto& race : races) {
+        bool wonLast = false;
+        int numWins = 0;
+
+        for (int i = 0; i < race.time/2 + race.time%2; i++) {
+            int currentRaceDistance = i * (race.time-i);
+
+            if (currentRaceDistance > race.recordDistance) {
+                numWins += 2;
+                wonLast = true;
+            } else {
+                wonLast = false;
+            }
+        }
+
+        if (wonLast && !(race.time%2)) {
+            numWins++;
+        }
+
+        winProducts *= numWins;
+    }
+
+    std::cout << "Product of ways you could have won:\n" << winProducts << std::endl;
+}
+
